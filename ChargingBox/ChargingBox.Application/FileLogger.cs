e97ca2675b0b2c9f73b4b62a47265e41f32a44ba
Pink-Wolf <44407859+Pink-Wolf@users.noreply.github.com>
2022-10-09 06:37:43 +0200

    Created and implemented FileLogger.

---
+++ ChargingBox/ChargingBox.Application/FileLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargingBox.Application
+{
+    public class FileLogger : ILogger
+    {
+        public string Filepath { get; set; }
+
+        public FileLogger(string filepath)
+        {
+            Filepath = filepath;
+        }
+
+        public void LogLock(bool lockState, object key, string filepath)
+        {
+            Filepath = filepath;
+        }
+
+        public void LogLock(bool lockState, object key, DateTime time)
+        {
+            byte[] text = new UTF8Encoding(true).GetBytes(GetLogString(lockState, key, time));
+
+            if (File.Exists(Filepath))
+            {
+                using FileStream stream = new(Filepath, FileMode.Append, FileAccess.Write);
+                stream.Write(text);
+            }
+            else
+            {
+                using FileStream stream = File.Create(Filepath);
+                stream.Write(text, 0, text.Length);
+            }
+        }
+        public void LogLock(bool lockState, object key) => LogLock(lockState, key, DateTime.Now);
+
+        public string GetLogString(bool lockState, object key, DateTime time)
+            => (lockState ? "Locked" : "Unlocked")
+                + $" with key {key} at {time:u}";
+        public string GetLogString(bool lockState, object key) => GetLogString(lockState, key, DateTime.Now);
+    }
+}
