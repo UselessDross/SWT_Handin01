b872c5871298f0763ff5f92c07c005b95b9610cd
Useless <202010014@post.au.dk>
2022-10-09 16:35:30 +0200

    Made a charger brance

---
+++ ChargingBox/ChargingBox.Implementation/ChargerControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingBox.Implementation
+{
+    public class ChargerControl : ICharger
+    {
+        public ChargerState State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsConnected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler<ChargerStateChangedEventArgs> StateChanged;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void OnStateChanged() 
+        {
+            throw new NotImplementedException(); 
+        }
+
+
+
+    }
+}
