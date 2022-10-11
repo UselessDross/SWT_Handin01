bba2960376c5a685535b330e2a20f063b45038aa
Useless <202010014@post.au.dk>
2022-10-11 18:56:45 +0200

    After much reasurch, an attempt on implementing buisnesslogic to an event has been added.
    - added 'OnStateChange()'
    - added a prototype difinition to what should be broadcasted when there is no connection;

--- ChargingBox/ChargingBox.Implementation/ChargerControl.cs
+++ ChargingBox/ChargingBox.Implementation/ChargerControl.cs
@@ -11,11 +11,28 @@ namespace ChargingBox.Implementation
         public ChargerState State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public bool IsConnected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+
         public event EventHandler<ChargerStateChangedEventArgs> StateChanged;
+
+        public void OnStateCHange()
+        {
+            ChargerStateChangedEventArgs newArg = new ChargerStateChangedEventArgs();
+            newArg.Before = State;
+            StateChanged.Invoke(this, newArg);
+        }
+
 
         public void Start()
         {
-            throw new NotImplementedException();
+            int phoneTemp = 2323; // just som numbers for the time while working on this.
+            if(phoneTemp == 0 || IsConnected == false) 
+            {
+                StateChanged += (s, args) =>
+                {
+                    State = ChargerState.Idle;
+                };
+                OnStateCHange();
+            }
         }
 
         public void Stop()
@@ -24,11 +41,8 @@ namespace ChargingBox.Implementation
         }
 
 
-        public void OnStateChanged() 
-        {
-            throw new NotImplementedException(); 
-        }
-
+        // thanks to this video. 
+        //https://www.youtube.com/watch?v=gYC-9PUGwDI
 
 
     }
