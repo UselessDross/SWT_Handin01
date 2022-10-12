407d5a22a6036961edc30cc974cc0236c5441f5b
Useless <202010014@post.au.dk>
2022-10-12 12:57:49 +0200

    added aditional conditons statements for setting the charger state.
    added a helper method, to make the code easer to manage.

--- ChargingBox/ChargingBox.Implementation/ChargerControl.cs
+++ ChargingBox/ChargingBox.Implementation/ChargerControl.cs
@@ -8,6 +8,16 @@ namespace ChargingBox.Implementation
 {
     public class ChargerControl : ICharger
     {
+
+        private void CharHelperMethod(ChargerState state_)
+        {
+            StateChanged += (s, args) =>
+            {
+                State = state_;
+            };
+            OnStateCHange();
+        }
+
         public ChargerState State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public bool IsConnected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
@@ -24,15 +34,20 @@ namespace ChargingBox.Implementation
 
         public void Start()
         {
-            int phoneTemp = 2323; // just som numbers for the time while working on this.
-            if(phoneTemp == 0 || IsConnected == false) 
-            {
-                StateChanged += (s, args) =>
-                {
-                    State = ChargerState.Idle;
-                };
-                OnStateCHange();
+            int phoneTemp = 2323;                                                 // just som numbers for the time while working on this.
+            if(phoneTemp == 0 || IsConnected == false) {                          // 0mA 
+                CharHelperMethod(ChargerState.Idle);
             }
+            else if(phoneTemp > 0 || phoneTemp <= 5 && IsConnected == true) {     // 0mA < ladestrøm <= 5mA (NOT 500mA)
+                CharHelperMethod(ChargerState.Charging);
+            }
+            else if (phoneTemp > 5 || phoneTemp <= 500 && IsConnected == true) {  // 5mA < ladestrøm <= 500mA
+                CharHelperMethod(ChargerState.FullyCharged);
+            }
+            else if (phoneTemp > 500) {                                           // ladestrøm > 500mA
+                CharHelperMethod(ChargerState.Error);
+            }
+
         }
 
         public void Stop()
