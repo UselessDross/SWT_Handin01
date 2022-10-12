bdade993ee03f01ee4430d30cb43f1bbfc56ee13
Useless <202010014@post.au.dk>
2022-10-12 15:48:47 +0200

    finally implemented the key reader

---
+++ ChargingBox/ChargingBox.Implementation2/Program.cs
@@ -0,0 +1,6 @@
+﻿using ChargingBox.Implementation2;
+using System;
+
+KeyReader mykey = new KeyReader();
+something myklass = new something(mykey);
+mykey.OnKeyRead(new ChargingBox.KeyReaderKeyReadEventArgs() { Key = 2 });
