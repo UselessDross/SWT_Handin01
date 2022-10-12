6b701d4f00f7b50eb5f0f29ccc26aa7033fcf7e2
Useless <202010014@post.au.dk>
2022-10-12 15:01:41 +0200

    finished implementing the charger class

--- ChargingBox/ChargingBox.Implementation/ChargerControl.cs
+++ ChargingBox/ChargingBox.Implementation/ChargerControl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,51 +9,54 @@ namespace ChargingBox.Implementation
 {
     public class ChargerControl : ICharger
     {
-
-        private void CharHelperMethod(ChargerState state_)
-        {
-            StateChanged += (s, args) =>
-            {
-                State = state_;
-            };
-            OnStateCHange();
-        }
-
-        public ChargerState State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsConnected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-
         public event EventHandler<ChargerStateChangedEventArgs> StateChanged;
+        private ChargerState _chargerState;
+        public bool IsConnected { get ; set ; }
 
-        public void OnStateCHange()
+        public ChargerState State {
+            get
+            {
+                return _chargerState;
+            } 
+            set
+            {
+                ChargerStateChangedEventArgs newArg = new ChargerStateChangedEventArgs();
+                newArg.Before = _chargerState;
+                newArg.After = _chargerState; 
+                _chargerState = value;
+                StateChanged?.Invoke(this, newArg);
+            } 
+        }
+    
+
+        private int ChargingMethod(int Phone)
         {
-            ChargerStateChangedEventArgs newArg = new ChargerStateChangedEventArgs();
-            newArg.Before = State;
-            StateChanged.Invoke(this, newArg);
+            return Phone + 1;
         }
 
 
         public void Start()
         {
-            int phoneTemp = 2323;                                                 // just som numbers for the time while working on this.
-            if(phoneTemp == 0 || IsConnected == false) {                          // 0mA 
-                CharHelperMethod(ChargerState.Idle);
+            int phoneTemp = 0;
+            State = ChargerState.Charging;
+            while ( State == ChargerState.Charging)
+            {
+                ChargingMethod(phoneTemp);
+                if (phoneTemp > 5 || phoneTemp <= 500 && IsConnected == true)
+                {
+                    State = ChargerState.FullyCharged;
+                }
+                else if (phoneTemp > 500 || IsConnected == false)
+                {
+                    State = ChargerState.Error;
+                }
             }
-            else if(phoneTemp > 0 || phoneTemp <= 5 && IsConnected == true) {     // 0mA < ladestrøm <= 5mA (NOT 500mA)
-                CharHelperMethod(ChargerState.Charging);
-            }
-            else if (phoneTemp > 5 || phoneTemp <= 500 && IsConnected == true) {  // 5mA < ladestrøm <= 500mA
-                CharHelperMethod(ChargerState.FullyCharged);
-            }
-            else if (phoneTemp > 500) {                                           // ladestrøm > 500mA
-                CharHelperMethod(ChargerState.Error);
-            }
-
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            IsConnected = false;
+            State = ChargerState.Idle;
         }
 
 
