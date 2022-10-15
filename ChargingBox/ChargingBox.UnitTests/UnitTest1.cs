ef1340b3a0946f73351b6e85e0e881712abdb247
Useless <202010014@post.au.dk>
2022-10-15 14:15:18 +0200

    added a time Nunit test

--- ChargingBox/ChargingBox.UnitTests/UnitTest1.cs
+++ ChargingBox/ChargingBox.UnitTests/UnitTest1.cs
@@ -20,13 +20,7 @@ namespace ChargingBox.UnitTests
         [SetUp]
         public void Setup()
         {
-            
-
-            uut = new FileLogger(path_);
-            
-
-            // what is presummed to be a virtual/mock dicreatory for the unit tests.
-            
+            uut = new FileLogger(path_);            
         }
 
         [TestCase("C:\\semesterFolders\\SWT\\SWTLabEx\\Lektion6\\ChargingBox\\LogTest.txt")]
@@ -43,18 +37,13 @@ namespace ChargingBox.UnitTests
         public void TestIs_LogLock_Correct(string a, string result)
         {
             // Arrange
-
             string data_;
             StreamReader reader = null;
-
+            
+            
             //ACT
             System.IO.File.WriteAllText(path_, string.Empty);
-           
-           uut.LogLock(true,a,path_);
-
-
-            //ASSERT
-
+            uut.LogLock(true,a,path_);
             try
             {
                 reader = new StreamReader(uut.Filepath);
@@ -70,11 +59,73 @@ namespace ChargingBox.UnitTests
             }
             
             
-                Assert.That(data_, Is.EqualTo(result+$" {DateTime.Now:u}"));
-
-
+            //ASSERT
+            Assert.That(data_, Is.EqualTo(result+$" {DateTime.Now:u}"));
         }
 
+        [TestCase("ALPHA", "Unlocked with key ALPHA at")]
+        [TestCase("BRAVO", "Unlocked with key BRAVO at")]
+        [TestCase("CHARLI", "Unlocked with key CHARLI at")]
+        public void TestIs_UnLogLock_Correct(string a, string result)
+        {
+            // Arrange
+            string data_;
+            StreamReader reader = null;
+
+
+            //ACT
+            System.IO.File.WriteAllText(path_, string.Empty);
+            uut.LogLock(false, a, path_);
+            try
+            {
+                reader = new StreamReader(uut.Filepath);
+                data_ = reader.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+
+            //ASSERT
+            Assert.That(data_, Is.EqualTo(result + $" {DateTime.Now:u}"));
+        }
+
+        [TestCase(1900, 10, 01, "1900-10-01 00:00:00Z")]
+        public void TestIs_LogLock_withTimespecification_Correct(int YYYY,int mm,int dd, string result)
+        {
+            // Arrange
+            string data_;
+            StreamReader reader = null;
+            DateTime testTime = new DateTime(YYYY, mm, dd);
+
+
+            //ACT
+            System.IO.File.WriteAllText(path_, string.Empty);
+            uut.LogLock(false, "ALPHA",testTime);
+            try
+            {
+                reader = new StreamReader(uut.Filepath);
+                data_ = reader.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+            data_ = data_.Replace("Unlocked with key ALPHA at ", "");
+
+            //ASSERT
+            Assert.That(data_, Is.EqualTo(result));
+        }
 
 
     }
