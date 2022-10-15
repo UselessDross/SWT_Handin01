db7a50e3002bf523b5c399a049308345b0bf74b3
Pink-Wolf <44407859+Pink-Wolf@users.noreply.github.com>
2022-10-15 18:25:03 +0200

    Removed the project Implementation2.

--- ChargingBox/ChargingBox.Implementation2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using ChargingBox.Implementation2;
-using System;
-
-KeyReader mykey = new KeyReader();
-something myklass = new something(mykey);
-mykey.OnKeyRead(new ChargingBox.KeyReaderKeyReadEventArgs() { Key = 2 });
