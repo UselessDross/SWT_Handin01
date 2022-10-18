4f07973515166f3795dc905abd31aed98c7f2823
Pink-Wolf <44407859+Pink-Wolf@users.noreply.github.com>
2022-10-18 21:59:50 +0200

    Changed KeyReader's namespcae to ChargingBox.Application.

--- ChargingBox/ChargingBox.Application/KeyReader.cs
+++ ChargingBox/ChargingBox.Application/KeyReader.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChargingBox.Implementation2
+namespace ChargingBox.Application
 {
     public class KeyReader : IKeyReader
     {
