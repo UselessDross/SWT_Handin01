40f3f8f26e2cee6b3af0314eb5d31c546892325b
Useless <202010014@post.au.dk>
2022-10-18 11:14:16 +0200

    CharingBox NUnit test now 100% complete and passed.

--- ChargingBox/ChargingBox.UnitTests/UnitTest1.cs
+++ ChargingBox/ChargingBox.UnitTests/UnitTest1.cs
@@ -1,11 +1,12 @@
 using ChargingBox.UnitTests.MockCLasses;
+using System.Reflection;
+
 
 namespace ChargingBox.UnitTests
 {
     /*
      * To test Console.Writeline, Use Console.SetOut  (Use StringWriter as the TextWriter)
      */
-
 
 
     public class Tests
@@ -157,30 +158,74 @@ namespace ChargingBox.UnitTests
 
 
 
-
-        public void UpdateDisplay_DisplaysCorrect()
+        [TestCase(ChargingBoxState.Available,ChargerState.Idle,         "Available\r\n")]
+        [TestCase(ChargingBoxState.Locked,   ChargerState.Idle,         "Available\r\n\r\n\r\n")]
+        [TestCase(ChargingBoxState.Locked,   ChargerState.Charging,     "Available\r\nCharging...\r\n\r\n")]
+        [TestCase(ChargingBoxState.Locked,   ChargerState.FullyCharged, "Available\r\nPhone fully charged\r\n\r\n")]
+        [TestCase(ChargingBoxState.Locked,   ChargerState.Error,        "Available\r\nError!\r\n\r\n")]
+        [TestCase(ChargingBoxState.Unlocked, ChargerState.Idle,         "Available\r\nRemember your phone!\r\n")]
+        [TestCase(ChargingBoxState.Error,    ChargerState.Idle,         "Available\r\nError!\r\n")]
+        public void UpdateDisplay_DisplaysCorrect(ChargingBoxState uutBaseState, 
+                                                  ChargerState chargerState_,
+                                                  string result_)
         {
             //ARRANGE
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            MockCharger   charger_   = new MockCharger();
             MockKeyReader keyReader_ = new MockKeyReader();
+
             uut = new ChargingBox(new MockDoor(),
-                                  new MockCharger(),
+                                  charger_,
                                   keyReader_,
                                   new MockDisplay(),
                                   new MockLogger());
-            uut.Charger.IsConnected = true;
-            uut.Door.IsOpen = false;
-            keyReader_.change_Key(KeyUsedToLockBox);
-            uut.State = uutBaseState;
-            keyReader_.changeStateEvent();
-            //Assert.That(uut.State, Is.EqualTo(ChargingBoxState.Locked));
 
+            uut.State      = uutBaseState;
+            charger_.State = chargerState_;
 
             //ACT
-            keyReader_.change_Key(KeyUsedToUnLockBox);
-            keyReader_.changeStateEvent();
+            if(uutBaseState == ChargingBoxState.Available)
+            {
+                uut.State = ChargingBoxState.Locked;
+                uut.Charger.IsConnected = true;
+                uut.Door.IsOpen = false;
+                keyReader_.change_Key("PlaceHolderKey");
+                keyReader_.changeStateEvent();
+            }
+            else if(uutBaseState == ChargingBoxState.Locked)
+            {
+                uut.State = ChargingBoxState.Available;
+                uut.Charger.IsConnected = true;
+                uut.Door.IsOpen = false;
+                keyReader_.change_Key("PlaceHolderKey");
+                keyReader_.changeStateEvent();
+            }
+            else if(uutBaseState == ChargingBoxState.Unlocked)
+            {
+                /*this was realized way to late in development. sorry*/
+                typeof(ChargingBox)
+                        .GetMethod(
+                                    "UpdateDisplay",
+                                    BindingFlags.NonPublic | BindingFlags.Instance)
+                        .Invoke(uut, null);
+            }
+            else
+            {
+                uut.State = uutBaseState;
+                typeof(ChargingBox)
+                       .GetMethod(
+                                   "UpdateDisplay",
+                                   BindingFlags.NonPublic | BindingFlags.Instance)
+                       .Invoke(uut, null);
+            }
+
+
+
 
             //ASSERT
-            Assert.That(uut.State, Is.EqualTo(result_));
+            Assert.That(stringWriter.ToString(), Is.EqualTo(result_));
+            
         }
 
 
